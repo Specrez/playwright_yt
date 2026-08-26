ce6d5b1a970b0355af6470103baa8601fc92fcb8
specrez <oshadha.dw@gmail.com>
2026-08-26 14:19:43 +0530

    using css selector

--- tests/selectors.spec.js
+++ tests/selectors.spec.js
@@ -4,9 +4,14 @@ test('Selectors Demo', async({page}) => {
 
     await page.goto('https://www.saucedemo.com/')
     await page.pause()
+    
     // using any object property
     await page.click('id=user-name')
     await page.locator('id=user-name').fill('Edison')
     await page.locator('[id="user-name"]').fill('Einstein')
+
+    // using css selector
+    // #login-button
+    await page.locator('#login-button').click()
 
 });
